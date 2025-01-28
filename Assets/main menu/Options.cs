0f481aa3b64a5721e234432e51a1cc1c80764f7b
lolzic123 <md54715@fer.hr>
2025-01-28 01:22:35 +0100

    dovrsene funkcionalnosti main menua
    glasnoca i mouse sensitivity

--- Assets/main menu/Options.cs
+++ Assets/main menu/Options.cs
@@ -2,25 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Options : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject optionsPanel;
-    public Slider volumeSlider;
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Slider mouseSlider;
     void Start()
     {
-        if (volumeSlider != null)
+        if (PlayerPrefs.HasKey("musicVolume"))
         {
-            volumeSlider.value = AudioListener.volume;
+            LoadVolume();
+        }
+        else
+        {
+            SetVolume();
+        }
+        if (PlayerPrefs.HasKey("mouseSensitivity"))
+        {
+            volumeSlider.value=PlayerPrefs.GetFloat("mouseSensitivity");
+        }
+        else
+        {
+            volumeSlider.value=0.5f;
         }
     }
     public void Back(){
         mainMenuPanel.SetActive(true);
+        Canvas.ForceUpdateCanvases();
         optionsPanel.SetActive(false);
     }
-    public void SetVolume(float volume)
+    public void SetVolume()
     {
-        AudioListener.volume = volume;
+        float volume= volumeSlider.value;
+        mixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadVolume(){
+        volumeSlider.value=PlayerPrefs.GetFloat("musicVolume");
+        mixer.SetFloat("music", Mathf.Log10(volumeSlider.value)*20);
+    }
+
+    public void SetMouse(){
+        float mouse= mouseSlider.value;
+        PlayerPrefs.SetFloat("mouseSensitivity", mouse);
+        PlayerPrefs.Save();
+    }
+    public void LoadMouse(){
+        mouseSlider.value=PlayerPrefs.GetFloat("mouseSensitivity");
     }
 }
