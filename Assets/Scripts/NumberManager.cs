b93a47bdd4a25bfcec9d9249c6a22e65a5aa287d
enadodig <ena.dodig@fer.hr>
2025-01-29 13:52:48 +0100

    import kartica

---
+++ Assets/Scripts/NumberManager.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberManager : MonoBehaviour
+{
+    public static List<int> AllGeneratedNumbers = new List<int>();
+
+    private static int addedCards = 0;       //radi debugganja, nije potrebno za funkcionalnost
+
+    public static void AddNumbers(int num1, int num2)
+    {
+        AllGeneratedNumbers.Add(num1);
+        AllGeneratedNumbers.Add(num2);
+
+        addedCards += 2;
+
+        if(addedCards == 6)
+            Debug.Log("sifra:" + string.Join("", AllGeneratedNumbers));     //radi debugganja
+    }
+}
+    
