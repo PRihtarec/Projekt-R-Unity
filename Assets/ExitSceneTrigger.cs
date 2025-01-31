71e205048e89447685950264f1ee3433309f43af
Marko <marko.grguric2@fer.unizg.hr>
2025-01-31 01:13:34 +0100

    Kad pobjedis te baca u main menu
    prebaci scenu na main menu, doduse ne postoji cursor

---
+++ Assets/ExitSceneTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitSceneTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            Debug.Log("Pobjeda!!!! Vraća se u main menu");
+            
+            SceneManager.LoadScene("main menu");
+        }
+    }
+}
