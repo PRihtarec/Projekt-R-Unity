52955275e4318177c0f53936f34545dd8783c9a0
Marko <marko.grguric2@fer.unizg.hr>
2025-01-31 11:37:58 +0100

    playerpickupdrop sound fix
    fixano da se cuje da se dropa nesto kad se ne dropa

--- Assets/DoorOpenTrigger.cs
+++ Assets/DoorOpenTrigger.cs
@@ -7,12 +7,20 @@ public class DoorOpenTrigger : MonoBehaviour
 {
     [SerializeField] private Door door;
     [SerializeField] private objectGrabbable Key;
+
+    private PlayerPickupDrop playerPickupDrop;
+
+    private void Start()
+    {
+        playerPickupDrop = FindObjectOfType<PlayerPickupDrop>();
+
+    }
     private void OnTriggerEnter(Collider other){
         Debug.Log("Kljuc u u ključanici");
         if (other.CompareTag("Key")){
             door.open();
-            objectGrabbable key = other.GetComponent<objectGrabbable>();
-            key.Drop();
+            objectGrabbable key = other.GetComponent<objectGrabbable>(); //kad se vrata otkljucaju dropam key na pod
+            playerPickupDrop.DropObject();
         }
     }
 }
